80ef901031fa738f9824c3600d8ceaa362523d87
cigar9 <cigar9.web@gmail.com>
2019-07-27 18:21:39 +0900

    firebaseにBasic認証を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,7 @@ module.exports = {
   },
 
   generate: {
+    dir: 'functions/static',
     routes(callback) {
       const projectPaths = ['city-s', 'ruf', 'canon']
       let routes = projectPaths.map(post => `/projects/${post}`)
